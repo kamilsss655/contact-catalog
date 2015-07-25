0a9b4163edb391c8a1132c36b008736f0fcb0a3b
kamilsos <kamilsos@gmail.com>
2015-07-25 16:36:32 +0000

    Button language fix

--- resources/views/contact/list.blade.php
+++ resources/views/contact/list.blade.php
@@ -46,7 +46,7 @@
                     <td data-title="Akcja" class="text-center">
                         
                         {!! Form::open(array('route' => array('contact.destroy', $contact->id), 'method' => 'delete', 'class'=> 'action-btn')) !!}
-                            <button type="submit" class="btn btn-danger btn-mini">Delete</button>
+                            <button type="submit" class="btn btn-danger btn-mini">Usuń</button>
                         {!! Form::close() !!}
                         <a href="{{ URL::to('contact/edit/' . $contact->id) }}" class="btn btn-mini btn-primary action-btn">Edytuj</a>
                         <a href="{{ URL::to('contact/' . $contact->id) }}" class="btn btn-mini btn-default action-btn">Pokaż</a>
