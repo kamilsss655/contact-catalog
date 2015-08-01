500c8fd9e7337115772c27093a79b5b94f6d4e73
kamilsos <kamilsos@gmail.com>
2015-08-01 15:37:22 +0000

    Fixed register form typo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,7 +26,7 @@
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label class="col-md-4 control-label">Adres e-mail</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 							</div>
